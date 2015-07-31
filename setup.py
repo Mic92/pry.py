03283ac9bcd5dccdd473a69db00f27b1d0b1ad22
Malo Skrylevo <majioa@yandex.ru>
2015-07-31 15:54:46 +0300

    Form the python module as an egg.

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Pry',
+    version='0.0.1',
+    description='Pry ruby-like interactive shell, written in Python',
+    author='Jörg Thalheim',
+    author_email='github@higgsboson.tk',
+    url='https://github.com/Mic92/pry.py',
+    py_modules=['pry'],
+    zip_safe=False,
+)
