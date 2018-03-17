de6cae778237742e3e7a0702e038f5a0fed51ca1
Jörg Thalheim <joerg@thalheim.io>
2018-03-17 09:04:07 +0000

    update email in setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,8 @@ setup(
     version='0.0.1',
     description='Pry ruby-like interactive shell, written in Python',
     author='Jörg Thalheim',
-    author_email='github@higgsboson.tk',
+    author_email='joerg@thaleheim.io',
     url='https://github.com/Mic92/pry.py',
     py_modules=['pry'],
-    zip_safe=False,
+    zip_safe=True,
 )
